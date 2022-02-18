bbf5c796bff02efa69b970ed183e6909fb188868
EC2 Default User <ec2-user@ip-172-31-82-27.ec2.internal>
2022-02-18 10:39:44 +0000

    [Finish] 課題2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get '' => "homes#top"
+  root to: 'homes#top'
+  # patch 'books/:id' => 'books#update', as: 'update_book'
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
